9b880ab116c92eac8d8ad4a862fb1d360fa4ee3a
root <root@ubuntuserver.Speedport_W_724V_01011603_06_003>
2020-08-04 15:08:12 +0200

    changed app text twice

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
-      <p className="App-into">
+      <p className="App-intro">
         I was changed on the Feature branch
       </p>
     </div>
